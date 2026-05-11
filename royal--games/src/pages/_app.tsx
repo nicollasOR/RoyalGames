ace0e3133641a48a7335ff003d0bef5eb9399849
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-05-11 09:07:08 -0300

    login iniciado

---
+++ royal--games/src/pages/_app.tsx
@@ -0,0 +1,34 @@
+import "@/styles/globals.css";
+import 'react-toastify/dist/ReactToastify.css';
+
+import type { AppProps } from "next/app";
+import { Exo_2, Orbitron, Inter  } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+
+const exo2 = Exo_2({
+  variable: "--font-exo_2",
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"]
+}); 
+
+const orbitron = Orbitron({
+  variable: "--font-orbitron",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"]
+  
+})
+
+const inter = Inter({
+  variable: "--font-inter",
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"]
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+  <main className={`${exo2.variable} ${orbitron.variable}`}>
+  <Component {...pageProps} />
+  <ToastContainer/>
+  </main>
+)}
+
---
+++ royal--games/src/pages/login/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./login.module.css";
+
+const Login = () => {
+  return (
+      <main className={`${styles.main} layout`}>
+        <img src="../img/png/mulher_login.png" id={styles.banner}alt="" />
+        <aside className={styles.lado_esquerdo}>
+          <img src="../svg/logo.svg" alt="" />
+          <form className={styles.logar}>
+            <div className={styles.inputs}>
+              <label htmlFor="Email">Email</label>
+              <input type="text" name="Email"/>
+            </div>
+            <div className={styles.inputs}>
+              <label htmlFor="Senha">Senha</label>
+              <input type="text" name="Senha" />
+            </div>
+
+            <button>Entrar</button>
+          </form>
+        </aside>
+      </main>
+  );
+};
+
+export default Login;
