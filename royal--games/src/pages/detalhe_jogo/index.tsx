92e1be11c86cf3471db3ead8b4b73a4481acc478
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-05-14 10:58:09 -0300

    __

--- royal--games/src/pages/detalhe_jogo/index.tsx
+++ royal--games/src/pages/detalhe_jogo/index.tsx
@@ -1,43 +1,45 @@
-import Header from '@/components/header/Header'
-import style from './detalhe.module.css'
-import Footer from '@/components/footer/Footer'
+import Header from "@/components/header/Header";
+import style from "./detalhe.module.css";
+import Footer from "@/components/footer/Footer";
 
-const detalhe = () =>{
-    
-    
-    return(
-        <>
-        <main className={style.main}>
-            <h1></h1>
-            <hr/>
-                <article className={style.container}>
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
-                </article>
-            <div>
-                <div>
+const detalhe = () => {
+  return (
+    <>
+      <Header />
+      <main className={style.main}>
+        <h1>Destalhes do jogo</h1>
+        <hr />
+        <article className={style.container}>
+          <img src="/img/png/lolzin.png" alt="" />
 
-                <span></span>
-                <p></p>
-                <span></span>
-                <p></p>
-                <span></span>
-                <p></p>
-                </div>
-                <div>
-                    <span></span>
-                    <p></p>
-                    <span></span>
-                    <p></p>
-                </div>
-            </div>
+          <div>
+            <h2>League Of Legends</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque a
+              illo quod quidem illum natus eaque id placeat sunt molestiae,
+              tenetur voluptas assumenda consequatur excepturi laboriosam rerum?
+              Veritatis id eaque, laudantium laborum nulla odit libero possimus
+              velit, est fugit, quia rerum voluptates aliquid. Veniam voluptatum
+              architecto, temporibus corporis aliquam ab?
+            </p>
+          </div>
+        </article>
+        <div className={style.informacoes}>
+          <aside className={style.lado_esq}>
+            <span>Classificação indicativa: 18 anos</span>
+            <span>Preço: </span>
+            <span>Plataformas: </span>
+          </aside>
+          <aside className={style.lado_dir}>
+            <span>Categorias:</span>
+            <span>Gêneros: </span>
+          </aside>
+        </div>
+      </main>
 
-        </main>        
-        
-        
-        </>
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
-export default detalhe
+export default detalhe;
--- royal--games/src/pages/jogo/index.tsx
+++ royal--games/src/pages/jogo/index.tsx
@@ -1,18 +1,73 @@
-import Footer from '@/components/footer/Footer'
-import styles from './produto.modules.css'
-import Header from '@/components/header/Header'
-import Lista from '@/components/lista/listaJogo'
+import Footer from "@/components/footer/Footer";
+import styles from "./jogo.module.css";
+import Header from "@/components/header/Header";
+import Lista from "@/components/lista/listaJogo";
 
-const Produto = () =>  {
+const Produto = () => {
+  return (
+    <>
+      <Header />
+      <section className={styles.cadastro}>
+        <h1>Cadastrar novo jogo</h1>
+        <hr />
+        <div className={styles.inserir_dados}>
+          <aside className={styles.lado_esq}>
+          <div className={styles.secao_inserir}>
+            <label htmlFor="nome">Nome</label>
+            <input id={styles.item1} type="text" name="nome"/>
+          </div>
+          <div className={styles.list}>
+          <div className={styles.secao_inserir}>
+            <label htmlFor="preco">Valor</label>
+            <input id={styles.item2} type="text" name="preco"/>
+          </div>
+          <div  className={styles.secao_inserir}>
+            <label htmlFor="">Gênero</label>
+           <select name="" id="" className={styles.opcoes}>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            </select>
+          </div>
+          <div  className={styles.secao_inserir}>
+            <label htmlFor="">Classificação Indicativa</label>
+            <select name="" id="" className={styles.opcoes}>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            </select>
 
-    return(
-        <>
-        <Header/>
-        
-        <Lista/>
-        <Footer/>
-        </>
-    )
-}
+          </div>
+          </div>
+        <div id={styles.inline}>
+          <div  className={styles.secao_inserir} id={styles.plataforma}>
+            <label htmlFor="">Plataforma</label>
+           <select name="" id="" className={styles.opcoes}>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            <option value="" className={styles.opcao}>fasdfasd</option>
+            </select>
+          </div>
+          <div className={styles.secao_inserir} id={styles.imagem}>
+            <label htmlFor="">Imagem</label>
+            <input id={styles.item6} type="file" />
+          </div>
+          </div>
+          </aside>
+          <aside className={styles.lado_dir}>
+          <div className={styles.secao_inserir}>
+            <label htmlFor="">Descrição</label>
+            <textarea id={styles.item7} name=""></textarea>
+          </div>
+          </aside>
 
-export default Produto
+        </div>
+        <button>Cadastrar</button>
+      </section>
+      <Lista />
+      <Footer />
+    </>
+  );
+};
+
+export default Produto;
